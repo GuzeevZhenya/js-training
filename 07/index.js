f8102517af17d40e318765829cc7044ce31cdd4e
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-10-05 16:39:10 +0300

    add

--- 07/index.js
+++ 07/index.js
@@ -30,25 +30,90 @@
 
 // console.log(rabbit.__proto__ == dog.__proto__);
 
-function Man() {
-    this.intro = function() {
-        alert(`i am ${this.name}`);
-    }
+// function Man() {
+//     this.intro = function() {
+//         alert(`i am ${this.name}`);
+//     }
+// }
+
+// User.prototype = new Man();
+
+// function User(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// const alex = new User('alex', 19);
+// alex.intro();
+// const mary = new User('reg', 12);
+// mary.intro();
+
+// Array.prototype.blablabla = function() {
+//     alert("bla-bla-bla");
+// }
+
+// const arr = [1, 2, 3];
+// arr.blablabla();
+
+
+// function Life(minDuration) {
+//     this.minDuration = minDuration;
+//     this.breeze = function() {
+//         alert(`I am Breezing! ${this.name}`);
+//     }
+// }
+
+
+// function Man(name, minDuration) {
+//     this.__proto__ = new Life(minDuration);
+//     this.name = name;
+//     this.intro = function() {
+//         alert(`i am ${this.name}`);
+//     }
+// }
+
+// // Man.prototype = new Life(18);
+// const man = new Man('alex', 18);
+// const man2 = new Man('Bob', 21);
+
+
+//Как создать объект, без поля прото и прототипа
+
+// const obj = Object.create(null);
+// console.log(obj);
+
+
+
+
+//DZ
+function Modal() {
+    this.show = function() {
+        console.log(`show ${this.name}`);
+    };
+    this.hide = function() {
+        console.log(`hide,${this.name}`);
+    };
 }
 
-User.prototype = new Man();
-
-function User(name, age) {
+function Warning(name, message) {
+    // this.__proto__ = new Modal();
     this.name = name;
-    this.age = age;
+    this.message = message;
 }
 
-const alex = new User('alex', 19);
-alex.intro();
-const mary = new User('reg', 12);
-mary.intro();
+function Success(name, message) {
+    // this.__proto__ = new Modal();
+    this.name = name;
+    this.message = message;
+}
 
-Array.prototype = function()
+Success.prototype = new Modal();
+Warning.prototype = new Modal();
 
-const arr = [1, 2, 3];
-const.blablabla();
+
+const warning = new Warning('404', 'erro');
+console.log(warning);
+
+
+const success = new Success('405', 'wef');
+console.log(success);
