7f764a53ee243d5c0a08c6c3a5e246abe15504a7
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-10-11 14:59:29 +0300

    Add

--- 09/index.js
+++ 09/index.js
@@ -3,7 +3,7 @@ const users = [{
         company: "Google",
         email: "alex @gmail.com",
         phone: "+375292929292",
-        balance: "$3",
+        balance: "$54",
         isActive: false,
         gender: "female",
     },
@@ -12,7 +12,7 @@ const users = [{
         company: "Yandex",
         email: "vlad @gmail.com",
         phone: "+375293429232",
-        balance: "$3",
+        balance: "$64",
         isActive: false,
         gender: "female",
     }, {
@@ -20,7 +20,7 @@ const users = [{
         company: "Google",
         email: "alex @gmail.com",
         phone: "+37529223492",
-        balance: "$3",
+        balance: "$57",
         isActive: false,
         gender: "female",
     }, {
@@ -28,77 +28,75 @@ const users = [{
         company: "Google",
         email: "alex @gmail.com",
         phone: "+375292929292",
-        balance: "$3",
+        balance: "$79",
         isActive: false,
-        gender: "female",
+        gender: "male",
     }, {
         name: "Sergei",
         company: "Google",
         email: "alex @gmail.com",
         phone: "+375292929292",
-        balance: "$3",
+        balance: "$43",
         isActive: false,
-        gender: "female",
+        gender: "male",
     }, {
         name: "Viktor",
         company: "Google",
         email: "alex @gmail.com",
         phone: "+375292929292",
-        balance: "$3",
+        balance: "$54",
         isActive: false,
-        gender: "female",
+        gender: "male",
     }, {
         name: "Zhenya",
         company: "Google",
         email: "alex @gmail.com",
         phone: "+375292929292",
-        balance: "$3",
+        balance: "$58",
         isActive: false,
         gender: "female",
     },
 ]
 
-function InfoTable(users) {
-    let table = document.createElement('table');
-    document.body.append(table);
+// function InfoTable(users) {
+//     let table = document.createElement('table');
+//     document.body.append(table);
 
-    table.classList.add('table_blur');
-    //Заголовки таблиц
-    let rowHeader = table.insertRow();
-    for (let prop in users[0]) {
-        let cell = rowHeader.insertCell();
-        cell.innerText = prop;
-        cell.classList.add('box');
-    }
-
-
-    //Инфо по таблице
-    for (let i = 0; i < users.length; i++) {
-        let row = table.insertRow();
-        for (let prop in users[i]) {
-            let cell = row.insertCell();
-            cell.classList.add('box');
-            cell.innerText = users[i][prop];
-        }
-        let btnDelete = document.createElement('button');
-        document.body.append(btnDelete);
-        btnDelete.innerHTML = 'Удалить';
-        // btnDelete.classList.add(`btn`+[i]);
+//     table.classList.add('table_blur');
+//     //Заголовки таблиц
+//     let rowHeader = table.insertRow();
+//     for (let prop in users[0]) {
+//         let cell = rowHeader.insertCell();
+//         cell.innerText = prop;
+//         cell.classList.add('box');
+//     }
 
 
-        btnDelete.addEventListener('click', (event) => {
-            event.preventDefault();
-            users.splice(i, 1);
-            console.log(users);
-            row.remove();
-            btnDelete.remove();
-            // let remove = this.closest(cell);
-            // remove.remove();
-        });
-    }
-}
+//     //Инфо по таблице
+//     for (let i = 0; i < users.length; i++) {
+//         let row = table.insertRow();
+//         for (let prop in users[i]) {
+//             let cell = row.insertCell();
+//             cell.classList.add('box');
+//             cell.innerText = users[i][prop];
+//         }
 
-InfoTable(users);
+
+//         let btnDelete = document.createElement('button');
+//         document.body.append(btnDelete);
+//         btnDelete.innerHTML = 'Удалить';
+
+//         btnDelete.addEventListener('click', (event) => {
+//             event.preventDefault();
+//             users.splice(i, 1);
+//             console.log(users);
+//             row.remove();
+//             btnDelete.remove();
+//         });
+//     }
+// }
+
+// InfoTable(users);
 
 // users.forEach(function(item, index) {
 
@@ -110,3 +108,43 @@ InfoTable(users);
 //     info.innerHTML = `${item.name},  ${item.company},  ${item.email}, ${item.phone}, ${item.balance}, ${item.isActive}, ${item.gender}`;
 //     // document.querySelector('body').appendChild(` < p > $ { info } < \p > `);
 // })
+
+const {
+    name,
+    company,
+    email,
+    phone,
+    balance,
+    isActive,
+    gender
+} = users;
+users.forEach((item) => {
+
+    let info = document.createElement('div');
+    document.body.appendChild(info);
+    info.classList.add('box');
+    info.innerHTML = `${item.name},  ${item.company},  ${item.email}, ${item.phone}, ${item.balance}, ${item.isActive}, ${item.gender}`;
+
+
+    let btnDelete = document.createElement('button');
+    document.body.append(btnDelete);
+    btnDelete.innerHTML = 'Удалить';
+    btnDelete.addEventListener('click', (event) => {
+        event.preventDefault();
+        users.splice(item, 1);
+        info.remove();
+        btnDelete.remove();
+        // item.remove();
+        // btnDelete.remove();
+        console.log(users);
+    });
+});
+
+const allWomen = (users) => users.filter((item) => item.gender === 'female');
+console.log(allWomen(users));
+
+const bigestBalance = (users) => users
+        .sort((a, b) => a.balance - b.balance);
+console.log(bigestBalance(users)[users.length-1]);
+
+
